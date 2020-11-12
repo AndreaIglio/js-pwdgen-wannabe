2b013808f128c59cfe39bb33abfc271f0ba78a19
Andrea Iglio <andrea.iglio@gmail.com>
2020-11-12 18:01:54 +0100

    Changed fixed year value with integer var

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,8 @@ var userLastname = prompt("Qual e' il tuo cognome?");
 
 var userColor = prompt("Qual e' il tuo colore preferito?");
 
+var currentYear = parseInt("20");
 
-document.getElementById("password").innerHTML = userName + userLastname + userColor + 19 ;
+document.getElementById("password").innerHTML = userName + userLastname + userColor + currentYear;
 
 // Variabili senza valore
