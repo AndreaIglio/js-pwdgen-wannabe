afbcc82460639db0ab4aad1bcdce027a8aa7d718
Andrea Iglio <andrea.iglio@gmail.com>
2020-11-12 15:51:45 +0100

    Completed Exercise

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
 // Variabili con valore
 
+var userName = prompt("Quale e il tuo nome?");
+
+var userLastname = prompt("Quale e il tuo cognome?");
+
+var userColor = prompt("Quale e il tuo colore preferito?");
+
+
+document.getElementById("password").innerHTML = userName + userLastname + userColor + 19 ;
 
 // Variabili senza valore
